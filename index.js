23872688e716ad21aeeedb0fa6b5205d8868942d
Lathifah Zumaemah <lathifahzumaemah@gmail.com>
2025-07-05 18:26:22 +0700

    moving index.js to root, update config and package files

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'build'))); // Serve frontend
+
+// Catch-all route for React SPA
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${PORT}`);
+});
